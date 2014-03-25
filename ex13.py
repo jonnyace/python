b2dc7b0ad5de88d73af06700f58d4f35a2aa55e2
jonnyace <jonthn@gmail.com>
2014-03-25 14:58:41 -0500

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "This script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+from sys import argv
+print "How many variables are there?"
+int(raw_input())
+
+

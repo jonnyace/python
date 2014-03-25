8fda2e03889b937ad27a7a08c267eb63811c7034
jonnyace <jonthn@gmail.com>
2014-03-25 14:51:28 -0500

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# This line prints the words
+print "I will now count my chickens:"
+# prints the word and then solves the problem
+print "Hens" , 25.0 + 30.0 /6
+#prints the word then solves .the problem
+print "Roosters" , 100.0 - 25.0  *3.0 % 4.0
+#merely shows the phrase
+print "Now I will count the eggs:"
+#prints three + two...etc
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# just prints the words
+print "Is it true that 3 + 2 < 5 - 7?"
+#actually solves the equation
+print 3 + 2 < 5 - 7
+#both prints and solves the problem
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+#merely prints the words
+print "Oh, that's why it's False."
+#yes, we'll take some more
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+
+print "Is it greater?", 5 > -2
+print "is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
